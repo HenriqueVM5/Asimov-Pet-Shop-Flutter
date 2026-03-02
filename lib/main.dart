348061dfb89e4d02718cde4d05cef2415838ee14
Lhferraaz <rickzzffx@gmail.com>
2026-03-02 01:55:19 -0300

    Adicionando o AuthProvider a arvore de widgets para que a autenticação com google funcione

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/login_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:pet_shop_app/providers/auth_provider.dart' as app_auth;
+import 'package:pet_shop_app/screens/cadastro_screen.dart';
 
 void main() async {
-  // inicializa os widgets do Flutter antes do Firebase
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Inicializa o Firebase 
-  await Firebase.initializeApp(); 
+  await Firebase.initializeApp();
 
   runApp(const PetShopApp());
 }
@@ -17,18 +16,22 @@ class PetShopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pet Shop Asimov',
-      // Configuração de Tema Global do App
-      theme: ThemeData(
-        primarySwatch: Colors.blue, // Substituir depois que escolher a cor
-        inputDecorationTheme: const InputDecorationTheme(
-          border: OutlineInputBorder(),
-          filled: true,
-          fillColor: Colors.white,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => app_auth.AuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Pet Shop Asimov',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          inputDecorationTheme: const InputDecorationTheme(
+            border: OutlineInputBorder(),
+            filled: true,
+            fillColor: Colors.white,
+          ),
         ),
+        home: const CadastroScreen(),
       ),
-      home: const LoginScreen(), // Define a tela inicial
     );
   }
 }
