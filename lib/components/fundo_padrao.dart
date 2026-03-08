66e340e4b785a709d3ee67f0c46263bb42e0972f
Lhferraaz <rickzzffx@gmail.com>
2026-03-08 20:03:27 -0300

    Commitando coisas que faltaram mas que não necessariamente são relevantes

--- lib/components/fundo_padrao.dart
+++ lib/components/fundo_padrao.dart
@@ -4,93 +4,84 @@ import 'package:flutter/material.dart';
 
 class FundoPadrao extends StatelessWidget {
   final Widget child;
-  final List<Widget> decoracoes; 
-  final Widget? bottomNavigationBar; 
+  final List<Widget> decoracoes;
+  final Widget? bottomNavigationBar;
 
   const FundoPadrao({
     super.key,
     required this.child,
-    this.decoracoes = const [], 
-    this.bottomNavigationBar, 
+    this.decoracoes = const [],
+    this.bottomNavigationBar,
   });
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      resizeToAvoidBottomInset: true, 
-      backgroundColor: Colors.transparent, 
-      extendBody: true, 
-      bottomNavigationBar: bottomNavigationBar, 
-      
-      //definição das cores de fundo
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.transparent,
+      extendBody: true,
+      bottomNavigationBar: bottomNavigationBar,
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topCenter, 
-            end: Alignment.bottomCenter, 
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
             colors: [
-              Color(0xFF8AD8FF), 
-              Colors.white, 
+              Color(0xFF8AD8FF),
+              Colors.white,
             ],
-            stops: [0.0, 0.75], 
+            stops: [0.0, 0.75],
           ),
         ),
-        
-        
         child: SafeArea(
-          bottom: false, // Deixa um espaço ente o fundo criado e a tela, para que quando a tab for adicionada não esteja colada
+          bottom: false,
           child: Center(
             child: FittedBox(
               fit: BoxFit.contain,
               child: SizedBox(
-                width: 375, // tamnho padrão do figma
-                height: 812, // tamanho padrão do figma
+                width: screenWidth,
+                height: screenHeight,
                 child: Stack(
                   clipBehavior: Clip.none,
                   children: [
-                    
-                    
                     // Onda azul no topo
                     Positioned(
-                      top: -95, 
-                      right: -70, 
+                      top: -100,
+                      right: -70,
                       child: Image.asset(
-                        "assets/images/Vector12.png", 
-                        width: 550, 
-                        height: 550, 
+                        "assets/images/Vector12.png",
+                        width: 550,
+                        height: 550,
                         fit: BoxFit.contain,
                       ),
                     ),
-
                     // Brinquedos do roda pé
                     Positioned(
                       bottom: 80,
                       left: -50,
                       right: 50,
                       child: Image.asset(
-                        "assets/images/Group11.png", 
-                        width: 70, 
-                        height: 70, 
+                        "assets/images/Group11.png",
+                        width: 70,
+                        height: 70,
                         fit: BoxFit.contain,
                       ),
                     ),
-
                     // Patinhas
                     Positioned(
-                      bottom: 140, 
-                      right: 90, 
+                      bottom: 140,
+                      right: 90,
                       child: Image.asset(
-                        "assets/images/pata_home.png", 
-                        width: 120, 
+                        "assets/images/pata_home.png",
+                        width: 120,
                       ),
                     ),
-
                     //decorações especificas de cada tela adicionadas posteriormente
                     ...decoracoes,
-
                     //child para colocar o conteudo da pagina
-                    child, 
-                    
+                    child,
                   ],
                 ),
               ),
--- lib/components/fundo_telas.dart
+++ lib/components/fundo_telas.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart'; 
+import 'package:google_fonts/google_fonts.dart';
 import 'fundo_padrao.dart';
 
 //codigo que add sobre o fundo_padão as decorações em comum das telas que não estão nos forms
@@ -7,11 +7,11 @@ import 'fundo_padrao.dart';
 class FundoTelas extends StatelessWidget {
   final Widget child;
   final Widget? bottomNavigationBar;
-  final String? titulo; 
+  final String? titulo;
 
   const FundoTelas({
-    super.key, 
-    required this.child, 
+    super.key,
+    required this.child,
     this.bottomNavigationBar,
     this.titulo,
   });
@@ -21,39 +21,36 @@ class FundoTelas extends StatelessWidget {
     return FundoPadrao(
       bottomNavigationBar: bottomNavigationBar,
       decoracoes: [
-        
         //Logo
         Positioned(
-          top: 60, 
-          left: 0, 
+          top: 60,
+          left: 0,
           right: 0,
           child: Center(
             child: Image.asset("assets/images/Header.png", height: 75),
           ),
         ),
 
-        
         // Pote de ração perto da logo
         Positioned(
-          top: 75, 
+          top: 75,
           right: 20,
           child: Image.asset("assets/images/pote_padrao.png", width: 80),
         ),
 
         // Bolinhas brancas abaixo do titulo
         Positioned(
-          top: 190, 
+          top: 190,
           left: 30,
           child: Image.asset("assets/images/bolinhas_branco.png", width: 75),
         ),
 
         // Saco de ração azul
         Positioned(
-          top: 200, 
+          top: 200,
           right: 35,
           child: Image.asset("assets/images/racao_ae.png", width: 70),
         ),
-
 
         //Titullo da tela
         if (titulo != null)
@@ -73,12 +70,13 @@ class FundoTelas extends StatelessWidget {
             ),
           ),
       ],
-      
 
       // Conteudo da tela vindo das telas proprias de cada aba
       // Paddin para que o conteudo seja escrito somente abaixo do titulo da tela
       child: Padding(
-        padding: const EdgeInsets.only(top: 200.0), // conteudo da tela aoarece desse ponto para baixo
+        padding: const EdgeInsets.only(
+          top: 200.0,
+        ), // conteudo da tela aoarece desse ponto para baixo
         child: child,
       ),
     );
